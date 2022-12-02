cc48c53909c56f3232e43878c9d6cb28a6a1d15a
Thomas Broadwater <tgbroadh2o@gmail.com>
2022-12-02 12:30:20 -0500

    Get jest initialized to work in new environment

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-undef */
+/* eslint-disable-next-line no-unused-vars */
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import '@testing-library/jest-dom';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders basic App structure', () => {
+  // render App component
   render(<App />);
+
+  // query DOM for text
   const linkElement = screen.getByText(/Logo Here/i);
   expect(linkElement).toBeInTheDocument();
 });
