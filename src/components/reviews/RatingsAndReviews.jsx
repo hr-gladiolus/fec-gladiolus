f586df96278141fed3c78ff03a0117ce7775f8bc
Cordelia Payson <cordelia.payson@gmail.com>
2022-12-05 14:17:09 -0500

    Connected RatingsAndReviews to App.sjx. Set up basic structure of RatingsAndReviews

--- src/components/reviews/RatingsAndReviews.jsx
+++ src/components/reviews/RatingsAndReviews.jsx
@@ -8,6 +8,16 @@ function RatingsAndReviews() {
   return (
     <div>
       <h1>Ratings & Reviews</h1>
+      <p>average rating number</p>
+      <p>star rating (insert when we actually get it done)</p>
+      <FilterRatings />
+      <ProductFactors />
+      <p>search for keyword</p>
+      <ReviewsList />
+      <button type="button">More Reviews</button>
+      <button type="button">Add A Review +</button>
     </div>
   );
 }
+
+export default RatingsAndReviews;
