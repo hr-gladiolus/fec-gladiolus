d27ad5a3bfbf9da019b041ee37a8f4ad92eaec02
Cordelia Payson <cordelia.payson@gmail.com>
2022-12-05 15:48:26 -0500

    set up GET request for product rating, rating showing on page

--- src/components/reviews/RatingsAndReviews.jsx
+++ src/components/reviews/RatingsAndReviews.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FilterRatings from './FilterRatings.jsx';
 import ProductFactors from './ProductFactors.jsx';
 import ReviewsList from './ReviewsList.jsx';
 import AddReview from './AddReview.jsx';
+import { getProductRating } from './api.js';
 
 function RatingsAndReviews() {
+  const [rating, setRating] = useState();
+
+  useEffect(() => {
+    getProductRating()
+      .then((result) => {
+        setRating(result);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Ratings & Reviews</h1>
-      <p>average rating number</p>
+      <p>{rating}</p>
       <p>star rating (insert when we actually get it done)</p>
       <FilterRatings />
       <ProductFactors />
