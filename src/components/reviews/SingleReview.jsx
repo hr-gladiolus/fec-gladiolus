bc2a18527bad15bbd644d92e370c0fa82709fa18
Cordelia Payson <cordelia.payson@gmail.com>
2022-12-06 09:49:39 -0500

    Complete basics of review list, single review, GET request for reviews

--- src/components/reviews/SingleReview.jsx
+++ src/components/reviews/SingleReview.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
 
 function SingleReview(props) {
-  // const { review } = props;
-  // const parseReview = JSON.parse(review);
-  // console.log(parseReview);
+  const { review } = props;
+  const parseReview = JSON.parse(review);
 
   const [showMore, setShowMore] = useState(false);
-
-  const parseReview = {
-    review_id: 1277210,
-    rating: 3,
-    summary: 'Such a great product!',
-    recommend: true,
-    response: null,
-    body: 'wow I really loved this product. It was perfect',
-    date: '2022-10-25T00:00:00.000Z',
-    reviewer_name: 'cordelia',
-    helpfulness: 2,
-    photos: [],
-  };
+  // this is my example review - leaving it in for future use
+  // const parseReview = {
+  //   review_id: 1277210,
+  //   rating: 3,
+  //   summary: 'Such a great product!',
+  //   recommend: true,
+  //   response: null,
+  //   body: 'wow I really loved this product. It was perfect',
+  //   date: '2022-10-25T00:00:00.000Z',
+  //   reviewer_name: 'cordelia',
+  //   helpfulness: 2,
+  //   photos: [],
+  // };
 
   function summaryOverflow() {
     const under60 = parseReview.summary.slice(0, 61);
@@ -57,7 +56,7 @@ function SingleReview(props) {
       {parseReview.summary.length > 60 ? summaryOverflow() : <h2>{parseReview.summary}</h2>}
       {parseReview.body.length > 250 ? bodyOverflow() : <p>{parseReview.body}</p>}
       {/* images here  */}
-      {parseReview.recommend === true && <p>I recommend this product</p>}
+      {parseReview.recommend === true ? <p>I recommend this product</p> : null}
       {parseReview.response && (
         <p>
           Response:
--- src/components/reviews/api.js
+++ src/components/reviews/api.js
@@ -35,7 +35,7 @@ export function getReviews() {
   return instance.get('/reviews/', {
     params: {
       page: 1,
-      count: 1,
+      count: 5,
       sort: 'newest',
       product_id: 37313,
     },
