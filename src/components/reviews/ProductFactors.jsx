4969836dd57d8ebf2710e3d0a864c76754ffcc7e
Cordelia Payson <cordelia.payson@gmail.com>
2022-12-08 11:12:48 -0500

    starting product factors setup

--- src/components/reviews/ProductFactors.jsx
+++ src/components/reviews/ProductFactors.jsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const Row = styled.div`
+  width: 100px;
+  display: flex;
+  flex-direction: row;
+
+  &:after {
+  content: "";
+  display: table;
+  clear: both;
+  }
+`;
+const SingleCharacteristic = styled.div`
+`;
+
+const CharacteristicName = styled.p`
+`;
+
+const Bar = styled.div`
+ width: 100%;
+  background-color: #858080;
+  text-align: center;
+  color: white;
+`;
+
+const LowerText = styled.div`
+`;
+
+const One = styled.div`
+  /* float: left; */
+`;
+
+const Five = styled.div`
+  /* float: right; */
+`;
 
 function ProductFactors() {
+  const [currentFactors, setCurrentFactors] = useState();
+  const [currentValues, setCurrentValues] = useState();
+  const product = useSelector((state) => state.product.productId);
+  const fakeExample = {
+    Fit: 3,
+    // Length: 3,
+    // Comfort: 3,
+    // Quality: 4,
+  };
+
+  const allFactors = {
+    Size: { one: 'A size too small', five: 'A size too wide' },
+    Width: { one: 'Too narrow', five: 'Too wide' },
+    Comfort: { one: 'Uncomfortable', five: 'Perfect' },
+    Quality: { one: 'Poor', five: 'Perfect' },
+    Length: { one: 'Runs Short', five: 'Runs long' },
+    Fit: { one: 'Runs tight', five: 'Runs long' },
+  };
+
+  const singleFactor = (factor) => (
+    <SingleCharacteristic key={factor}>
+      <CharacteristicName>
+        {factor}
+      </CharacteristicName>
+      <Bar>bar</Bar>
+      <LowerText>
+        <One>{allFactors[factor].one}</One>
+        <Five>{allFactors[factor].five}</Five>
+      </LowerText>
+    </SingleCharacteristic>
+  );
   return (
-    <div>
-      <h3>Product Factors</h3>
-    </div>
+    <Row>
+      {Object.keys(fakeExample).map((factor) => singleFactor(factor))}
+    </Row>
   );
 }
 
