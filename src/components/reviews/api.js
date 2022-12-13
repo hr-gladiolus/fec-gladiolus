4119f2274efe6d2aff185f536fdf0bfa7356b441
Cordelia Payson <cordelia.payson@gmail.com>
2022-12-12 19:04:56 -0500

    removed unnecessary API requests

--- src/components/reviews/api.js
+++ src/components/reviews/api.js
@@ -9,62 +9,6 @@ const instance = axios.create({
 
 // eslint-disable-next-line import/prefer-default-export
 
-// gets product id
-export function getProductRating(productId) {
-  return instance.get('/reviews/meta', {
-    params: {
-      product_id: productId,
-    },
-  })
-    .then((response) => {
-      const { ratings } = response.data;
-      let total = 0;
-      let average = 0;
-      let ratingsNumber = 0;
-      for (let i = 1; i < 6; i += 1) {
-        const currentRating = parseInt(ratings[i], 10);
-        total += currentRating * i;
-        ratingsNumber += currentRating;
-      }
-      average = total / ratingsNumber;
-      const rounded10 = Math.round(average * 10) / 10;
-      return rounded10;
-    })
-    .catch((err) => err);
-}
-
-// gets product ratings and number of ratings
-export function getRatings(productId) {
-  return instance.get('/reviews/meta', {
-    params: {
-      product_id: productId,
-    },
-  })
-    .then((response) => {
-      const oldFormatRatings = response.data.ratings;
-      const ratingsString = {
-        five: oldFormatRatings['5'],
-        four: oldFormatRatings['4'],
-        three: oldFormatRatings['3'],
-        two: oldFormatRatings['2'],
-        one: oldFormatRatings['1'],
-      };
-      let numberOfRatings = 0;
-
-      for (let i = 0; i < Object.values(ratings).length; i += 1) {
-        const currentRating = parseInt(Object.values(ratings)[i], 10);
-        numberOfRatings += currentRating;
-      }
-
-      const result = {
-        number: numberOfRatings,
-        ratings,
-      };
-      return result;
-    })
-    .catch((err) => err);
-}
-
 // gets reviews
 export function getReviews(productId, sortOption) {
   return instance.get('/reviews/', {
