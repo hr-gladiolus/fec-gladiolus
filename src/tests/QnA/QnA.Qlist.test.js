13b461a4a961cca2ec9d5435c6bdb0b4063122a8
Timothy Williams <williamstim146@gmail.com>
2022-12-13 18:58:39 -0600

    Build out basic tests for the QnA component.

--- src/tests/QnA/QnA.Qlist.test.js
+++ src/tests/QnA/QnA.Qlist.test.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { waitFor } from '@testing-library/dom';
 import QnA from '../../components/QnA/QnA.jsx';
 import store from '../../store/store';
+import testData from './testData';
 
-test('renders basic QnA structure', () => {
-  // render App component
-  render(<Provider store={store}><QnA /></Provider>);
-
-  // query DOM for text
-  const linkElement = screen.getByText(/QUESTIONS & ANSWERS/i);
-  expect(linkElement).toBeInTheDocument();
+describe('testing Search component', () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <QnA />
+      </Provider>,
+    );
+  });
+  afterEach(() => {
+    cleanup();
+    jest.clearAllMocks();
+  });
+  test('renders basic QnA structure', () => {
+    // query DOM for text
+    const linkElement = screen.getByText(/QUESTIONS & ANSWERS/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
--- src/tests/QnA/QnA.test.js
+++ src/tests/QnA/QnA.test.js
@@ -6,11 +6,34 @@ import { waitFor } from '@testing-library/dom';
 import QnA from '../../components/QnA/QnA.jsx';
 import store from '../../store/store';
 
-test('renders basic QnA structure', () => {
-  // render App component
-  render(<Provider store={store}><QnA /></Provider>);
-
-  // query DOM for text
-  const linkElement = screen.getByText(/QUESTIONS & ANSWERS/i);
-  expect(linkElement).toBeInTheDocument();
+describe('testing Search component', () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <QnA />
+      </Provider>,
+    );
+  });
+  afterEach(() => {
+    cleanup();
+    jest.clearAllMocks();
+  });
+  test('renders basic QnA structure', () => {
+    // query DOM for text
+    const linkElement = screen.getByText(/QUESTIONS & ANSWERS/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('renders the search component to the screen', async () => {
+    const searchInput = screen.findByPlaceholderText('HAVE QUESTIONS? SEARCH FOR ANSWERS...');
+    expect(await searchInput).toBeInTheDocument();
+  });
+  test('renders the Qlist component to the screen', async () => {
+    const QsForEachQuestion = screen.findAllByText(/Q:/);
+    expect(await QsForEachQuestion).toBeTruthy();
+    expect(await QsForEachQuestion).toHaveLength(2);
+  });
+  test('renders the More component', async () => {
+    const AddQuestion = screen.findByText('ADD QUESTION +');
+    expect(await AddQuestion).toBeInTheDocument();
+  });
 });
