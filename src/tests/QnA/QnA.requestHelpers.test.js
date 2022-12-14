23cc6e53334862fd766f11f5f3d62da88191d25c
Timothy Williams <williamstim146@gmail.com>
2022-12-13 19:01:05 -0600

    Add the requestHelpers test file.

---
+++ src/tests/QnA/QnA.requestHelpers.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { waitFor } from '@testing-library/dom';
